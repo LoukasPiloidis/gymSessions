f9e1de06a69147c2d901344f22c25baa9de47682
Loukas Piloidis <loukas.piloidis@ui.com>
2023-09-17 18:35:43 +0200

    test

--- src/assets/checkmark.tsx
+++ src/assets/checkmark.tsx
@@ -1,6 +1,6 @@
 import { palette } from "../palette";
 
-const checkmark = () => {
+const Checkmark = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -14,4 +14,4 @@ const checkmark = () => {
   );
 };
 
-export default checkmark;
+export default Checkmark;
